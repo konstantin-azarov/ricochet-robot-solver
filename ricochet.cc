5c7bea15dcc5df4b5e77c9c58044e54798eca9b2
Konstantin Azarov <konstantin.azarov@gmail.com>
2017-01-01 20:50:09 -0800

    WORKS!

--- ricochet.cc
+++ ricochet.cc
@@ -1,5 +1,7 @@
 #include <cmath>
 #include <opencv2/opencv.hpp>
+#include <map>
+#include <queue>
 
 struct Line {
   float angle;
@@ -216,17 +218,30 @@ cv::Mat_<cv::Vec2f> computeMap(cv::Mat_<double> m, int w, int h) {
   return res;
 }
 
+const double kInf = 100000;
+
 struct Cell {
   bool located = false;
   int cx = -1, cy = -1, w, h;
 
-  bool right_wall = false, left_wall = false;
+  double intensity = 0;
+
+  double w_left = kInf, w_right = kInf, w_top = kInf, w_bottom = kInf;
 };
 
 const int kMapCells = 16;
 
 struct Cells {
   Cell c[kMapCells][kMapCells];
+};
+
+struct Walls {
+  bool vert[kMapCells][kMapCells + 1];
+  bool hor[kMapCells+1][kMapCells];
+};
+
+struct Robot {
+  int i, j;
 };
 
 Cells extractCells(
@@ -343,75 +358,172 @@ Cells extractCells(
   return cells;
 }
 
-struct ColorStat {
-  int mean[3];
-  int var[3];
-};
+Walls detectWalls(
+    const cv::Mat_<uint8_t>& img_g,
+    Cells& cells) {
+  Walls walls;
 
-void detectWalls(cv::Mat_<cv::Vec3b>& img_rgb, Cells& cells) {
   cv::Mat blurred; 
-  cv::blur(img_rgb, blurred, cv::Size(5, 5));
+  cv::blur(img_g, blurred, cv::Size(2, 2));
  
-  cv::Mat_<uint8_t> mask(img_rgb.rows, img_rgb.cols, (uint8_t)0);
+  cv::Mat_<uint8_t> mask(img_g.rows, img_g.cols, (uint8_t)0);
 
   for (int i = 0; i < kMapCells; ++i) {
     for (int j = 0; j < kMapCells; ++j) {
       auto& c = cells.c[i][j];
-      if (c.located) {
+      if (true) {
         int x0 = c.cx - c.w*0.3;
         int x1 = c.cx + c.w*0.3;
         int y0 = c.cy - c.h*0.3;
         int y1 = c.cy + c.h*0.3;
 
-        mask(cv::Range(y0, y1), cv::Range(x0, x1)) = 255;
+        c.intensity = cv::mean(
+            blurred(cv::Range(y0, y1), cv::Range(x0, x1)))[0];
       }
     }
   }
 
-  /* cv::imshow("mask", mask); */
-  /* cv::waitKey(-1); */
-
-  std::vector<double> baseline_mean, baseline_std_dev; 
-  cv::meanStdDev(img_rgb, baseline_mean, baseline_std_dev, mask);
-
-  std::vector<double> mean, std_dev;
-
-  for (int i = 0; i < kMapCells - 1; ++i) {
-    for (int j = 0; j < kMapCells - 1; ++j) {
+  for (int i = 0; i < kMapCells; ++i) {
+    for (int j = 0; j < kMapCells; ++j) {
       auto& c1 = cells.c[i][j];
       auto& c2 = cells.c[i][j+1];
-      // right
-      int x0 = (c1.cx + c2.cx)/2 - 5;
-      int x1 = x0 + 5;
-      int y0 = std::max(c1.cy - c1.h/2, c2.cy - c2.h/2);
-      int y1 = std::min(c1.cy + c1.h/2, c2.cy + c2.h/2);
+      auto& c3 = cells.c[i+1][j];
 
-      cv::meanStdDev(
-          img_rgb(cv::Range(y0, y1), cv::Range(x0, x1)),
-          mean, std_dev);
+      double intensity_r = kInf;
+      double intensity_b = kInf;
 
-      bool good = false;
-      for (int c = 0; c < 3 && !good; c++) {
-        double d = abs(mean[c] - baseline_mean[c]);
-        if (d > std_dev[c]*2 && d > baseline_std_dev[c]*2) {
-          good = true;
+      if (j < kMapCells - 1) {
+        // right
+        int x0 = (c1.cx + c2.cx)/2 - 10;
+        int x1 = x0 + 20;
+        int y0 = std::max(c1.cy - c1.h/2, c2.cy - c2.h/2);
+        int y1 = std::min(c1.cy + c1.h/2, c2.cy + c2.h/2);
+
+        intensity_r = cv::mean( 
+            blurred(cv::Range(y0, y1), cv::Range(x0, x1)))[0];
+      }
+
+      if (i < kMapCells - 1) {
+        // bottom
+        int y0 = (c1.cy + c3.cy)/2 - 10;
+        int y1 = y0 + 20;
+        int x0 = std::max(c1.cx - c1.w/2, c3.cx - c3.w/2);
+        int x1 = std::min(c1.cx + c1.w/2, c3.cx + c3.w/2);
+
+        intensity_b = cv::mean( 
+            blurred(cv::Range(y0, y1), cv::Range(x0, x1)))[0];
+      }
+
+      int n_r = 0, n_b = 0, n = 0;
+      for (int ii = i - 1; ii <= i + 2; ++ii) {
+        for (int jj = j - 1; jj <= j + 2; ++jj) {
+          if (ii >= 0 && jj >= 0 && ii < kMapCells && jj < kMapCells) {
+            auto& c = cells.c[ii][jj]; 
+            n++;
+            if (intensity_r < c.intensity * 0.9 || intensity_r < c.intensity - 30) {
+              n_r++;
+            }
+            
+            if (intensity_b < c.intensity * 0.9 || intensity_b < c.intensity - 30) {
+              n_b++;
+            }
+          }
         }
       }
 
-      c1.right_wall = good;
-      c2.left_wall = good;
+      if (n_r > n * 0.6) {
+        c1.w_right = intensity_r;
+        c2.w_left = intensity_r;
+      }
+
+      if (n_b > n * 0.6) {
+        c1.w_bottom = intensity_b;
+        c3.w_top = intensity_b;
+      }      
+     
+      if (j > 0 && j < kMapCells - 1 && (i < 7 || i > 8 || j < 6 || j > 9)) {
+        if (c1.w_left < c1.w_right) {
+          c1.w_right = kInf;
+        } else {
+          c1.w_left = kInf;
+        }
+      }
+
+      if (i > 0 && i < kMapCells - 1 && (j < 7 || j > 8 || i < 6 || i > 9)) {
+        if (c1.w_bottom < c1.w_top) {
+          c1.w_top = kInf;
+        } else {
+          c1.w_bottom = kInf;
+        }
+      }
+    }
+
+  }
+
+  for (int i=0; i < kMapCells; ++i) {
+    for (int j = 0; j < kMapCells - 1; ++j) {
+      auto& c1 = cells.c[i][j];
+      auto& c2 = cells.c[i][j+1];
+      if ((c1.w_right < kInf) !=  (c2.w_left < kInf)) {
+        c1.w_right = kInf;
+        c2.w_left = kInf;
+      }
     }
   }
-  /* std::cout << mean << std::endl; */
-  /* std::cout << std_dev << std::endl; */
+
+  for (int j=0; j < kMapCells; ++j) {
+    for (int i = 0; i < kMapCells - 1; ++i) {
+      auto& c1 = cells.c[i][j];
+      auto& c2 = cells.c[i+1][j];
+      if ((c1.w_bottom < kInf) !=  (c2.w_top < kInf)) {
+        c1.w_bottom = kInf;
+        c2.w_top = kInf;
+      }
+    }
+  }
+
+  for (int i = 0;  i < kMapCells; ++i) {
+    walls.vert[i][0] = walls.vert[i][kMapCells] = true;
+    for (int j = 0; j < kMapCells - 1; ++j) {
+      walls.vert[i][j+1] = cells.c[i][j].w_right < kInf;
+    }
+  }
+
+  walls.vert[7][7] = walls.vert[7][9] = true;
+  walls.vert[8][7] = walls.vert[8][9] = true;
+
+  for (int j = 0;  j < kMapCells; ++j) {
+    walls.hor[0][j] = walls.hor[kMapCells][j] = true;
+    for (int i = 0; i < kMapCells - 1; ++i) {
+      walls.hor[i+1][j] = cells.c[i][j].w_bottom < kInf;
+    }
+  }
+ 
+  walls.hor[7][7] = walls.hor[9][7] = true;
+  walls.hor[7][8] = walls.hor[9][8] = true;
+
+  return walls;
 }
 
-struct Robot {
-  int cx, cy, r;
-};
+std::pair<int, int> findCell(const Cells& cells, int cx, int cy) {
+  std::pair<int, int> res;
+  for (int i=0; i < kMapCells; i++)
+    for (int j=0; j < kMapCells; ++j) {
+      if (cells.c[i][j].cx - cells.c[i][j].w < cx && 
+          cells.c[i][j].cx + cells.c[i][j].w > cx &&
+          cells.c[i][j].cy - cells.c[i][j].h < cy && 
+          cells.c[i][j].cy + cells.c[i][j].h > cy) {
+        res.first = i;
+        res.second = j;
+      }
+    }
+
+  return res;
+}
 
 Robot findRobot(
     cv::Mat img_hsv, 
+    const Cells& cells,
     uint8_t hue, uint8_t hue_delta, uint8_t min_sat, uint8_t min_value,
     int expected_size) {
   cv::Mat_<uint8_t> mask;
@@ -444,7 +556,7 @@ Robot findRobot(
     }
   }
 
-  Robot res { -1, -1, -1 };
+  Robot res { -1, -1 };
 
   if (best_i != -1) {
     int x0 = stats(best_i, cv::CC_STAT_LEFT);
@@ -452,9 +564,12 @@ Robot findRobot(
     int w = stats(best_i, cv::CC_STAT_WIDTH);
     int h = stats(best_i, cv::CC_STAT_HEIGHT);
 
-    res.cx = x0 + w/2;
-    res.cy = y0 + h/2;
-    res.r = std::max(w/2, h/2);
+    int cx = x0 + w/2;
+    int cy = y0 + h/2;
+
+    auto ind = findCell(cells, cx, cy);
+    res.i = ind.first;
+    res.j = ind.second;
   }
   
   /* cv::imshow("hsv", img_hsv); */
@@ -464,7 +579,7 @@ Robot findRobot(
   return res;
 }
 
-void drawCells(cv::Mat& img, const Cells& cells) {
+void drawCells(cv::Mat& img, const Cells& cells, const Walls& walls) {
   for (int i = 0; i < kMapCells; ++i) {
     for (int j = 0; j < kMapCells; ++j) {
       auto& c = cells.c[i][j];
@@ -477,12 +592,39 @@ void drawCells(cv::Mat& img, const Cells& cells) {
             c.located ? cv::Scalar(0, 255, 0) : cv::Scalar(0, 0, 255),
             1);
 
-        if (c.right_wall) {
+        if (walls.vert[i][j]) {
+          cv::line(
+              img,
+              cv::Point2i(c.cx - c.w/2, c.cy - c.h/2),
+              cv::Point2i(c.cx - c.w/2, c.cy + c.h/2),
+              cv::Scalar(0, 255, 255),
+              2);
+        }
+        
+        if (walls.vert[i][j+1]) {
           cv::line(
               img,
               cv::Point2i(c.cx + c.w/2, c.cy - c.h/2),
               cv::Point2i(c.cx + c.w/2, c.cy + c.h/2),
-              cv::Scalar(0, 0, 0),
+              cv::Scalar(0, 255, 255),
+              2);
+        }
+
+        if (walls.hor[i][j]) {
+          cv::line(
+              img,
+              cv::Point2i(c.cx - c.w/2, c.cy - c.h/2),
+              cv::Point2i(c.cx + c.w/2, c.cy - c.h/2),
+              cv::Scalar(0, 255, 255),
+              2);
+        }
+        
+        if (walls.hor[i+1][j]) {
+          cv::line(
+              img,
+              cv::Point2i(c.cx - c.w/2, c.cy + c.h/2),
+              cv::Point2i(c.cx + c.w/2, c.cy + c.h/2),
+              cv::Scalar(0, 255, 255),
               2);
         }
       }
@@ -490,14 +632,38 @@ void drawCells(cv::Mat& img, const Cells& cells) {
   }
 }
 
-void drawRobot(cv::Mat img, const Robot& robot, cv::Scalar color) {
-  cv::circle(img, cv::Point2i(robot.cx, robot.cy), robot.r, color, 2);
+void drawRobot(
+    cv::Mat img, 
+    const Cells& cells, 
+    const Robot& robot, 
+    cv::Scalar color) {
+
+  const auto& c = cells.c[robot.i][robot.j];
+
+  cv::circle(
+      img, 
+      cv::Point2i(c.cx, c.cy), 
+      std::min(c.w/2, c.h/2), 
+      color, 
+      2);
 }
 
 const int kMapSize = 512;
 
-int main(int argc, char** argv) {
-  auto img_raw = cv::imread(argv[1]);
+enum RobotColor {
+  YELLOW,
+  GREEN,
+  RED,
+  BLUE
+};
+
+void processImage(
+    const std::string& filename, 
+    Cells& cells, 
+    Walls* walls,
+    Robot* robots,
+    cv::Mat* rectified_image) {
+  auto img_raw = cv::imread(filename);
 
   cv::Mat_<cv::Vec3b> img_bgr, img_hsv;
   cv::resize(img_raw, img_bgr, cv::Size(kWidth, kHeight)); 
@@ -505,12 +671,13 @@ int main(int argc, char** argv) {
 
   auto rect = findRectangle(img_bgr, 10, 60);
 
-  cv::line(img_bgr, rect[0], rect[1], cv::Scalar(0, 255, 0));
-  cv::line(img_bgr, rect[1], rect[2], cv::Scalar(0, 255, 0));
-  cv::line(img_bgr, rect[2], rect[3], cv::Scalar(0, 255, 0));
-  cv::line(img_bgr, rect[3], rect[0], cv::Scalar(0, 255, 0));
+  /* cv::line(img_bgr, rect[0], rect[1], cv::Scalar(0, 255, 0)); */
+  /* cv::line(img_bgr, rect[1], rect[2], cv::Scalar(0, 255, 0)); */
+  /* cv::line(img_bgr, rect[2], rect[3], cv::Scalar(0, 255, 0)); */
+  /* cv::line(img_bgr, rect[3], rect[0], cv::Scalar(0, 255, 0)); */
   
   /* cv::imshow("debug", img_bgr); */ 
+  /* cv::waitKey(-1); */
 
   std::vector<cv::Point2i> target_rect {
     cv::Point2i(0, 0),
@@ -528,6 +695,7 @@ int main(int argc, char** argv) {
   cv::remap(img_bgr, img_r_bgr, map, cv::noArray(), cv::INTER_LINEAR);
 
   /* cv::imshow("rectified", img_r); */
+  /* cv::waitKey(-1); */
 
   cv::Mat_<uint8_t> img_g, edges;
   cv::extractChannel(img_r, img_g, 2);
@@ -541,38 +709,296 @@ int main(int argc, char** argv) {
       edges, edges, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3)));
   
   float nominal_size = kMapSize / kMapCells;
-  auto cells = extractCells(
+  cells = extractCells(
       edges, 
       nominal_size, nominal_size*0.5, nominal_size*1.2, 
       0.2, 0.9, 0.5);
 
   // Walls
-  detectWalls(img_r_bgr, cells);
-
-  cv::Mat dbg_img;
-  img_r_bgr.copyTo(dbg_img);
-  //cv::cvtColor(edges, dbg_img, cv::COLOR_GRAY2BGR); 
-
-  drawCells(dbg_img, cells);
+  if (walls) {
+    *walls = detectWalls(img_g, cells);
+  }
 
   // Robots
-  int min_robot_value = 90;
-  int robot_size = kMapSize / kMapCells;
+  if (robots) {
+    int min_robot_value = 90;
+    int robot_size = kMapSize / kMapCells;
 
-  auto yellow = findRobot(img_r, 20, 20, 150, min_robot_value, robot_size);
-  auto green = findRobot(img_r, 60, 20, 150, min_robot_value, robot_size);
-  auto red = findRobot(img_r, 180, 20, 150, min_robot_value, robot_size);
-  auto blue = findRobot(img_r, 110, 20, 150, min_robot_value, robot_size);
+    robots[YELLOW] = findRobot(
+        img_r, cells, 20, 20, 150, min_robot_value, robot_size);
+    robots[GREEN] = findRobot(
+        img_r, cells, 60, 20, 150, min_robot_value, robot_size);
+    robots[RED] = findRobot(
+        img_r, cells, 180, 20, 150, min_robot_value, robot_size);
+    robots[BLUE] = findRobot(
+        img_r, cells, 110, 20, 150, min_robot_value, robot_size);
+  }
 
-  drawRobot(dbg_img, yellow, cv::Scalar(0, 255, 255));
-  drawRobot(dbg_img, green, cv::Scalar(0, 255, 0));
-  drawRobot(dbg_img, red, cv::Scalar(0, 0, 255));
-  drawRobot(dbg_img, blue, cv::Scalar(255, 0, 0));
+  if (rectified_image) {
+    *rectified_image = img_r_bgr;
+  }
+}
 
+struct Map {
+  Cells cells;
+  Walls walls;
+  Robot robots[4];
+  cv::Mat img;
+
+  int selected_robot = -1;
+  std::pair<int, int> destination = std::make_pair(-1, -1);
+
+  std::vector<std::pair<int, Robot> > solution; 
+};
+
+struct Pos {
+  Robot robots[4];
+
+  bool operator < (const Pos& b) const {
+    for (int i=0; i < 4; ++i) {
+      if (robots[i].i != b.robots[i].i) {
+        return robots[i].i < b.robots[i].i;
+      }
+
+      if (robots[i].j != b.robots[i].j) {
+        return robots[i].j < b.robots[i].j;
+      }
+    }
+
+    return false;
+  }
+};
+
+std::ostream& operator << (std::ostream& s, const Pos& p) {
+  for (int i = 0; i < 4; ++i) {
+    s << p.robots[i].i << " " << p.robots[i].j << " ";
+  }
+
+  return s;
+}
+
+struct Rec {
+  int distance;
+  int moved_robot;
+  Robot prev_pos;
+};
+
+std::ostream& operator << (std::ostream& s, const Rec& r) {
+  s << "D=" << r.distance << "; R=" << r.moved_robot << "; p=(" 
+    << r.prev_pos.i << ", " << r.prev_pos.j << ")";
+  return s;
+}
+
+bool isWall(const Walls& w, int i, int j, int di, int dj) {
+  if (dj == 1) {
+    return w.vert[i][j+1];
+  } else if (dj == -1) {
+    return w.vert[i][j];
+  } else if (di == 1) {
+    return w.hor[i+1][j];
+  } else if (di == -1) {
+    return w.hor[i][j];
+  }
+}
+
+bool occupied(int i, int j, Robot* rs, int k) {
+  for (int kk=0; kk < 4; ++kk) {
+    if (kk != k) {
+      if (rs[kk].i == i && rs[kk].j == j) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+Pos moveRobot(const Walls& w, Robot* rs, int k, int di, int dj) {
+  Robot r = rs[k]; 
+
+  while (!isWall(w, r.i, r.j, di, dj) && !occupied(r.i + di, r.j + dj, rs, k)) {
+    r.i += di;
+    r.j += dj;
+  }
+
+  Pos res;
+  res.robots[k] = r;
+  for (int i=0; i < 4; ++i) {
+    if (i != k) {
+      res.robots[i] = rs[i];
+    }
+  }
+
+  return res;
+}
+
+void solve(Map& m) {
+  std::map<Pos, Rec> positions;
+  std::queue<Pos> queue;
+
+  Pos p0 { { m.robots[0], m.robots[1], m.robots[2], m.robots[3] } };
+  positions[p0] = Rec { 0, -1, { -1, -1 } };
+
+  queue.push(p0);
+
+  Pos final_pos;
+
+  const int di[4] = { 1, -1, 0, 0 };
+  const int dj[4] = { 0, 0, 1, -1 };
+
+  while (!queue.empty()) {
+    Pos p = queue.front();
+    queue.pop();
+    const Rec& r = positions[p];
+
+    if (p.robots[m.selected_robot].i == m.destination.first &&
+        p.robots[m.selected_robot].j == m.destination.second) {
+      final_pos = p;
+      break;
+    }
+
+    for (int k=0; k < 4; ++k) {
+      for (int j=0; j < 4; ++j) {
+        Pos p1 = moveRobot(m.walls, p.robots, k, di[j], dj[j]);
+
+       // std::cout << "M: " << k << " " << j << "; " << p << " -> " << p1 << std::endl;
+
+        if (!positions.count(p1)) {
+          auto r1 = Rec { r.distance + 1, k, p.robots[k] };
+         // std::cout << r1 << std::endl;
+          positions[p1] = r1; 
+          queue.push(p1);
+        }
+      }
+    }
+  }
+
+  std::cout << "Final: " << final_pos << std::endl;
+  std::cout << "Rec: " << positions[final_pos] << std::endl;
+
+  m.solution.clear();
+
+  Pos p = final_pos;
+  while (true) {
+    auto r = positions[p];
+    if (r.moved_robot == -1) {
+      break;
+    }
+    m.solution.push_back(std::make_pair(r.moved_robot, p.robots[r.moved_robot]));
+    p.robots[r.moved_robot] = r.prev_pos;
+    std::cout << "MOVE: " << r.moved_robot << " " << r.distance << " " << p << std::endl;
+  }
+
+  std::reverse(m.solution.begin(), m.solution.end());
+}
+
+cv::Scalar colors[4] = {
+  cv::Scalar(0, 255, 255),
+  cv::Scalar(0, 255, 0),
+  cv::Scalar(0, 0, 255),
+  cv::Scalar(255, 0, 0)
+};
+
+void renderMap(const Map& m) {
+  cv::Mat dbg_img;
+
+  m.img.copyTo(dbg_img);
+
+  drawCells(dbg_img, m.cells, m.walls);
+  for (int i=0; i < 4; ++i) {
+    drawRobot(dbg_img, m.cells, m.robots[i], colors[i]);
+  }
+
+  if (m.selected_robot != -1) {
+    drawRobot(
+        dbg_img, m.cells, m.robots[m.selected_robot], 
+        cv::Scalar(255, 255, 255));
+  }
+
+  if (m.destination.first != -1) {
+    const auto& c = m.cells.c[m.destination.first][m.destination.second];
+    cv::line(
+        dbg_img, 
+        cv::Point2i(c.cx - c.w/2, c.cy - c.h/2),
+        cv::Point2i(c.cx + c.w/2, c.cy + c.h/2),
+        cv::Scalar(255, 255, 255),
+        2);
+
+    cv::line(
+        dbg_img, 
+        cv::Point2i(c.cx - c.w/2, c.cy + c.h/2),
+        cv::Point2i(c.cx + c.w/2, c.cy - c.h/2),
+        cv::Scalar(255, 255, 255),
+        2);
+  }
+
+  Robot r[4];
+
+  for (int i=0; i < 4; ++i) {
+    r[i] = m.robots[i];
+  }
+
+  for (const auto& s : m.solution) {
+    auto c1 = m.cells.c[r[s.first].i][r[s.first].j];
+    auto c2 = m.cells.c[s.second.i][s.second.j];
+
+    r[s.first] = s.second;
+
+    cv::line(
+        dbg_img,
+        cv::Point2d(c1.cx, c1.cy),
+        cv::Point2d(c2.cx, c2.cy),
+        colors[s.first],
+        2);
+  }
+
+  cv::imshow("map", dbg_img);
+}
+
+void onMouse(int event, int x, int y, int, void* ptr) {
+  Map* m = static_cast<Map*>(ptr);
+
+  if (event != cv::EVENT_LBUTTONUP) {
+    return;
+  }
   
+  auto ind = findCell(m->cells, x, y);
+  int found_robot = -1;
 
-  cv::imshow("edges", dbg_img);
-  cv::waitKey(-1);
+  for (int i=0; i < 4; ++i) {
+    if (m->robots[i].i == ind.first && m->robots[i].j == ind.second) {
+      found_robot = i;
+      break;
+    }
+  }
+
+  if (found_robot != -1) {
+    m->selected_robot = found_robot;
+  } else {
+    m->destination = findCell(m->cells, x, y);
+  }
+
+  if (m->selected_robot != -1 && m-> destination.first != -1) {
+    solve(*m);
+  }
+
+  renderMap(*m);
+}
+
+int main(int argc, char** argv) {
+  Map m;
+
+  processImage(argv[1], m.cells, &m.walls, nullptr, nullptr);
+  processImage(argv[2], m.cells, nullptr, m.robots, &m.img);
+
+
+  renderMap(m);
+  cv::setMouseCallback("map", onMouse, &m);
+  while (true) {
+    int c = cv::waitKey(-1);
+    if (c == 27) {
+      break;
+    }
+  }
 
   return 0;
 }
